19209d3b1accdb6b74bff590db56707de0ce74bd
smanrique1629 <160699139+smanrique1629@users.noreply.github.com>
2024-03-07 22:58:27 -0500

    Update main.js
    Added show more function js

--- assets/js/main.js
+++ assets/js/main.js
@@ -306,6 +306,13 @@
 			});
 
 		// Events.
+				function showMore(){
+			    //removes the link
+			    document.getElementById('link').parentElement.removeChild('link');
+			    //shows the #more
+			    document.getElementById('more').style.display = "block";
+			}
+	
 			$body.on('click', function(event) {
 
 				// Article visible? Hide.
